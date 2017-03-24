fd5fb75567107ff1fbbfc659dc039de41a44483d
gekun <gekun@cn.ibm.com>
2017-03-24 12:51:28 +0800

    merge master and ch3

--- first.sh
+++ first.sh
@@ -4,5 +4,4 @@ echo "change something else after the first line"
 
 echo "make anohter change on master branch, again"
 
-echo "change 1 for master"
-echo "change 2 for master"
+echo "merge change 1/2 for master and ch3"
